63467a477924d0f5fb910884b744f5e55521b455
Mario Susanto <msus0004@student.monash.edu>
2023-06-15 23:40:36 +0800

    added the account table and modified the schema accordingly

--- databaseGen/script.js
+++ databaseGen/script.js
@@ -55,6 +55,7 @@ function createTables() {
     runAndOutput("DROP TABLE audit")
         .then(()=>runAndOutput("DROP TABLE transaction"))
         .then(()=>runAndOutput("DROP TABLE category"))
+        .then(()=>runAndOutput("DROP TABLE account"))
         .then(()=>runAndOutput("DROP TABLE user"))
         .then(()=>runAndOutput(
             `
@@ -69,10 +70,22 @@ function createTables() {
         .then(()=>runAndOutput(`ALTER TABLE user ENGINE = InnoDB;`))
         .then(()=>runAndOutput(
             `
+            CREATE TABLE account (
+                acc_id INT(8) NOT NULL AUTO_INCREMENT,
+                acc_name VARCHAR(50) NOT NULL,
+                acc_start_amount DOUBLE NOT NULL,
+                user_id INT(8) NOT NULL,
+                PRIMARY KEY (acc_id)
+            )
+            `
+        ))
+        .then(()=>runAndOutput(`ALTER TABLE account ENGINE = InnoDB;`))
+        .then(()=>runAndOutput(`ALTER TABLE account ADD CONSTRAINT fk_user_account FOREIGN KEY ( user_id ) REFERENCES user ( user_id )`))
+        .then(()=>runAndOutput(
+            `
             CREATE TABLE category (
                 cat_id INT(8) NOT NULL AUTO_INCREMENT,
                 cat_name VARCHAR(50) NOT NULL,
-                cat_start_amount DOUBLE NOT NULL,
                 user_id INT(8) NOT NULL,
                 PRIMARY KEY (cat_id)
             )
@@ -86,14 +99,16 @@ function createTables() {
                 trans_id INT(8) NOT NULL AUTO_INCREMENT,
                 trans_date DATE NOT NULL,
                 trans_amount DOUBLE NOT NULL,
+                acc_id INT(8) NOT NULL,
                 cat_id INT(8) NOT NULL,
-                trans_desc INT(8) NOT NULL,
+                trans_desc VARCHAR(250),
                 trans_transfer_id INT(8),
                 PRIMARY KEY (trans_id)
             )
             `
         ))
         .then(()=>runAndOutput(`ALTER TABLE transaction ENGINE = InnoDB;`))
+        .then(()=>runAndOutput(`ALTER TABLE transaction ADD CONSTRAINT fk_account_transaction FOREIGN KEY ( acc_id ) REFERENCES account ( acc_id )`))
         .then(()=>runAndOutput(`ALTER TABLE transaction ADD CONSTRAINT fk_category_transaction FOREIGN KEY ( cat_id ) REFERENCES category ( cat_id )`))
         .then(()=>runAndOutput(`ALTER TABLE transaction ADD CONSTRAINT fk_transaction_transaction FOREIGN KEY ( trans_transfer_id ) REFERENCES transaction ( trans_id )`))
         .then(()=>runAndOutput(
@@ -102,12 +117,14 @@ function createTables() {
                 audit_id INT(8) NOT NULL AUTO_INCREMENT,
                 audit_date DATE NOT NULL,
                 audit_amount DOUBLE NOT NULL,
-                cat_id INT(8) NOT NULL,
+                acc_id INT(8) NOT NULL,
                 PRIMARY KEY (audit_id)
             )
             `
         ))
         .then(()=>runAndOutput(`ALTER TABLE audit ENGINE = InnoDB;`))
-        .then(()=>runAndOutput(`ALTER TABLE audit ADD CONSTRAINT fk_category_audit FOREIGN KEY ( cat_id ) REFERENCES category ( cat_id )`))
+        .then(()=>runAndOutput(`ALTER TABLE audit ADD CONSTRAINT fk_account_audit FOREIGN KEY ( acc_id ) REFERENCES account ( acc_id )`))
 }
+
+
 createTables();
